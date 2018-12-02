d7282817f44f8890d4fc939d3c7614a9085f5c34
MATHEUS MINARI MATIAS <matheus.matias01@fatec.sp.gov.br>
2018-12-01 23:23:01 -0200

    UCT de Registro Estoque

---
+++ lancelot-sistema-contabilidade/uct_registra_estoque.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class uct_registra_estoque
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txt_nome = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btn_registrar = New System.Windows.Forms.Button()
+        Me.lbl_id = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txt_nome
+        '
+        Me.txt_nome.BackColor = System.Drawing.Color.FromArgb(CType(CType(42, Byte), Integer), CType(CType(42, Byte), Integer), CType(CType(42, Byte), Integer))
+        Me.txt_nome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txt_nome.Font = New System.Drawing.Font("Tw Cen MT", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_nome.ForeColor = System.Drawing.SystemColors.Menu
+        Me.txt_nome.Location = New System.Drawing.Point(227, 273)
+        Me.txt_nome.Margin = New System.Windows.Forms.Padding(2)
+        Me.txt_nome.Name = "txt_nome"
+        Me.txt_nome.Size = New System.Drawing.Size(406, 28)
+        Me.txt_nome.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tw Cen MT", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(223, 249)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(148, 22)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nome do estoque"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.FromArgb(CType(CType(36, Byte), Integer), CType(CType(164, Byte), Integer), CType(CType(100, Byte), Integer))
+        Me.Label6.Font = New System.Drawing.Font("Tw Cen MT", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(82, 57)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Padding = New System.Windows.Forms.Padding(3)
+        Me.Label6.Size = New System.Drawing.Size(213, 37)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Registrar estoque"
+        '
+        'btn_registrar
+        '
+        Me.btn_registrar.BackColor = System.Drawing.Color.FromArgb(CType(CType(36, Byte), Integer), CType(CType(164, Byte), Integer), CType(CType(100, Byte), Integer))
+        Me.btn_registrar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.btn_registrar.FlatAppearance.BorderSize = 4
+        Me.btn_registrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_registrar.Font = New System.Drawing.Font("Tw Cen MT", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_registrar.ForeColor = System.Drawing.Color.White
+        Me.btn_registrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btn_registrar.Location = New System.Drawing.Point(293, 388)
+        Me.btn_registrar.Margin = New System.Windows.Forms.Padding(2)
+        Me.btn_registrar.Name = "btn_registrar"
+        Me.btn_registrar.Size = New System.Drawing.Size(274, 50)
+        Me.btn_registrar.TabIndex = 14
+        Me.btn_registrar.Text = "Confirmar Registro"
+        Me.btn_registrar.UseVisualStyleBackColor = False
+        '
+        'lbl_id
+        '
+        Me.lbl_id.BackColor = System.Drawing.Color.FromArgb(CType(CType(42, Byte), Integer), CType(CType(42, Byte), Integer), CType(CType(42, Byte), Integer))
+        Me.lbl_id.Font = New System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_id.ForeColor = System.Drawing.SystemColors.Menu
+        Me.lbl_id.Location = New System.Drawing.Point(227, 197)
+        Me.lbl_id.Name = "lbl_id"
+        Me.lbl_id.Size = New System.Drawing.Size(116, 28)
+        Me.lbl_id.TabIndex = 32
+        Me.lbl_id.Text = "ID"
+        Me.lbl_id.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tw Cen MT", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(223, 173)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 22)
+        Me.Label2.TabIndex = 33
+        Me.Label2.Text = "ID do estoque"
+        '
+        'uct_registra_estoque
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(234, Byte), Integer), CType(CType(234, Byte), Integer), CType(CType(234, Byte), Integer))
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lbl_id)
+        Me.Controls.Add(Me.btn_registrar)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_nome)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "uct_registra_estoque"
+        Me.Size = New System.Drawing.Size(860, 624)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt_nome As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btn_registrar As Button
+    Friend WithEvents lbl_id As Label
+    Friend WithEvents Label2 As Label
+End Class
